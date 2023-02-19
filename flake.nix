8459f7b83cd9dad1378cfb357ea3c842f2cffe11
Ethan Brooks <ethanabrooks@gmail.com>
2023-02-19 14:30:17 -0500

    cleanup dependencies

--- flake.nix
+++ flake.nix
@@ -25,59 +25,9 @@
       overrides = pyfinal: pyprev: let
         inherit (pyprev) buildPythonPackage fetchPypi;
       in rec {
-        ale-py = pyprev.ale-py.overridePythonAttrs (let
-          roms = fetchTarball {
-            url = "https://roms8.s3.us-east-2.amazonaws.com/Roms.tar.gz";
-            sha256 = "sha256:0g9xffdm7zndf84m14f1j1x1v3ybm7ls7498071xlhah9k80bskq";
-          };
-        in {
-          postInstall = ''
-            export LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
-            $out/bin/ale-import-roms ${roms}
-          '';
-        });
-        astunparse = pyprev.astunparse.overridePythonAttrs (old: {
-          buildInputs = (old.buildInputs or []) ++ [pyfinal.wheel];
-        });
-        clu = buildPythonPackage rec {
-          pname = "clu";
-          version = "0.0.7";
-          src = fetchPypi {
-            inherit pname version;
-            sha256 = "sha256-RJqa8XnDpcRPwYlH+4RKAOos0x4+3hMWf/bv6JNn2ys=";
-          };
-          buildInputs = with pyfinal; [
-            absl-py
-            etils
-            flax
-            jax
-            jaxlib
-            ml-collections
-            numpy
-            packaging
-            tensorflow
-            tensorflow-datasets
-          ];
-        };
-        etils = pyprev.etils.overridePythonAttrs (old: {
-          propagatedBuildInputs =
-            builtins.filter (i: i.pname != "etils") old.propagatedBuildInputs;
-        });
         # Use cuda-enabled jaxlib as required
         jaxlib = pyprev.jaxlibWithCuda.override {
           inherit (pyprev) absl-py flatbuffers numpy scipy six;
-        };
-        ml-collections = buildPythonPackage rec {
-          pname = "ml_collections";
-          version = "0.1.1";
-          src = fetchPypi {
-            inherit pname version;
-            sha256 = "sha256-P+/McuxDOqHl0yMHo+R0u7Z/QFvoFOpSohZr/J2+aMw=";
-          };
-          buildInputs = with pyfinal; [absl-py contextlib2 pyyaml six];
-          prePatch = ''
-            export HOME=$TMPDIR;
-          '';
         };
         ray = pyprev.ray.overridePythonAttrs (old: {
           propagatedBuildInputs =
