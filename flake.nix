b7dc08af2ca02a562bbe33e3fcf832d0c0b0fef3
Ethan Brooks <ethanabrooks@gmail.com>
2023-03-29 14:40:50 -0400

    slim down nbconvert override

--- flake.nix
+++ flake.nix
@@ -34,84 +34,16 @@
           inherit (pyprev) absl-py flatbuffers numpy scipy six;
         };
 
-        nbconvert = let
-          # see https://github.com/jupyter/nbconvert/issues/1896
-          style-css = fetchurl {
-            url = "https://cdn.jupyter.org/notebook/5.4.0/style/style.min.css";
-            hash = "sha256-WGWmCfRDewRkvBIc1We2GQdOVAoFFaO4LyIvdk61HgE=";
+        nbconvert = pyprev.nbconvert.overridePythonAttrs (old: {
+          format = "pyproject";
+
+          src = fetchPypi {
+            inherit (old) pname version;
+            hash = "sha256-ju1nvYMU8+yHxDUcL2dK86BOWJCrkF1r2SfAWuwc8n0=";
           };
-        in
-          pyprev.nbconvert.overridePythonAttrs (old: {
-            pname = "nbconvert";
-            version = "7.2.3";
 
-            format = "pyproject";
-
-            src = fetchPypi {
-              inherit (old) pname version;
-              hash = "sha256-ju1nvYMU8+yHxDUcL2dK86BOWJCrkF1r2SfAWuwc8n0=";
-            };
-
-            # Add $out/share/jupyter to the list of paths that are used to search for
-            # various exporter templates
-            patches = [
-              ./templates.patch
-            ];
-
-            postPatch = ''
-              substituteAllInPlace ./nbconvert/exporters/templateexporter.py
-              mkdir -p share/templates/classic/static
-              cp ${style-css} share/templates/classic/static/style.css
-            '';
-
-            nativeBuildInputs = with pyfinal; [
-              hatchling
-            ];
-
-            propagatedBuildInputs = with pyfinal;
-              [
-                beautifulsoup4
-                bleach
-                defusedxml
-                jinja2
-                #jupyter_core
-                jupyterlab-pygments
-                markupsafe
-                mistune
-                nbclient
-                packaging
-                pandocfilters
-                pygments
-                tinycss2
-                traitlets
-              ]
-              ++ lib.lists.optionals (pythonOlder "3.10") [
-                importlib-metadata
-              ];
-
-            preCheck = ''
-              export HOME=$(mktemp -d)
-            '';
-
-            checkInputs = with pyfinal; [
-              ipywidgets
-              pyppeteer
-              pytestCheckHook
-            ];
-            doCheck = false;
-
-            disabledTests = [
-              # Attempts network access (Failed to establish a new connection: [Errno -3] Temporary failure in name resolution)
-              "test_export"
-              "test_webpdf_with_chromium"
-              # ModuleNotFoundError: No module named 'nbconvert.tests'
-              "test_convert_full_qualified_name"
-              "test_post_processor"
-            ];
-
-            # Some of the tests use localhost networking.
-            __darwinAllowLocalNetworking = true;
-          });
+          nativeBuildInputs = old.nativeBuildInputs ++ [pyprev.hatchling];
+        });
         ray = pyprev.ray.overridePythonAttrs (old: {
           propagatedBuildInputs = (old.propagatedBuildInputs or []) ++ [pyfinal.pandas];
         });
