e26ebd5c13bfbe81735febe967c6510db89fa001
Ethan Brooks <ethanabrooks@gmail.com>
2023-03-29 14:47:51 -0400

    more cleanup

--- flake.nix
+++ flake.nix
@@ -17,14 +17,9 @@
           cudaSupport = true;
         };
       };
-      inherit (pkgs) bash buildEnv cudaPackages dockerTools linuxPackages mkShell poetry2nix python39 stdenv fetchurl lib;
-      inherit (poetry2nix) mkPoetryApplication mkPoetryEnv;
-      inherit (cudaPackages) cudatoolkit;
-      inherit (linuxPackages) nvidia_x11;
-      python = python39;
-      overrides = pyfinal: pyprev: let
-        inherit (pyprev) buildPythonPackage fetchPypi;
-      in rec {
+      inherit (pkgs) mkShell poetry2nix lib;
+      python = pkgs.python39;
+      overrides = pyfinal: pyprev: rec {
         ipywidgets = pyprev.ipywidgets.overridePythonAttrs (old: {
           preferWheel = true;
         });
@@ -37,7 +32,7 @@
         nbconvert = pyprev.nbconvert.overridePythonAttrs (old: {
           format = "pyproject";
 
-          src = fetchPypi {
+          src = pyprev.fetchPypi {
             inherit (old) pname version;
             hash = "sha256-ju1nvYMU8+yHxDUcL2dK86BOWJCrkF1r2SfAWuwc8n0=";
           };
@@ -60,7 +55,7 @@
           # poetry2nix version so that rpath is set correctly.
           pyprev.pytorch-bin.overridePythonAttrs (old: {
             inherit (old) pname version;
-            src = fetchurl {
+            src = pkgs.fetchurl {
               url = "https://download.pytorch.org/whl/cu116/torch-1.13.1%2Bcu116-cp39-cp39-linux_x86_64.whl";
               sha256 = "sha256-20V6gi1zYBO2/+UJBTABvJGL3Xj+aJZ7YF9TmEqa+sU=";
             };
@@ -75,45 +70,22 @@
         preferWheels = true;
         overrides = poetry2nix.overrides.withDefaults overrides;
       };
-      poetryEnv = mkPoetryEnv poetryArgs;
+      poetryEnv = poetry2nix.mkPoetryEnv poetryArgs;
       buildInputs = with pkgs; [
         alejandra
-        coreutils
-        nodePackages.prettier
         poetry
         poetryEnv
       ];
     in rec {
       devShell = mkShell rec {
         inherit buildInputs;
-        PYTHONFAULTHANDLER = 1;
         PYTHONBREAKPOINT = "ipdb.set_trace";
-        LD_LIBRARY_PATH = "${nvidia_x11}/lib";
+        LD_LIBRARY_PATH = "${pkgs.linuxPackages.nvidia_x11}/lib";
         shellHook = ''
           set -o allexport
           source .env
           set +o allexport
         '';
-      };
-      packages.default = dockerTools.buildImage {
-        name = "ppo";
-        tag = "latest";
-        copyToRoot =
-          buildEnv
-          {
-            name = "image-root";
-            pathsToLink = ["/bin" "/ppo"];
-            paths = buildInputs ++ [pkgs.git ./.];
-          };
-        config = {
-          Env = with pkgs; [
-            "PYTHONFAULTHANDLER=1"
-            "PYTHONBREAKPOINT=ipdb.set_trace"
-            "LD_LIBRARY_PATH=/usr/lib64/"
-            "PATH=/bin:$PATH"
-          ];
-          Cmd = ["${poetryEnv.python}/bin/python"];
-        };
       };
     };
   in
